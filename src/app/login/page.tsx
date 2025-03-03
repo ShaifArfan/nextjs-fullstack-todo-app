9c68ceb3aea810ad968559fb548cf893bafdd879
Arfan <shaifarfan08@gmail.com>
2025-03-04 01:18:39 +0600

    login update

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,14 +3,21 @@ import React from "react";
 
 function Page() {
   return (
-    <form
-      action={async () => {
-        "use server";
-        await signIn("github", { redirectTo: "/" });
-      }}
-    >
-      <button type="submit">Signin with GitHub</button>
-    </form>
+    <div className="container flex justify-center">
+      <form
+        action={async () => {
+          "use server";
+          await signIn("github", { redirectTo: "/" });
+        }}
+      >
+        <button
+          className="bg-slate-600 p-1 w-[200px] mt-5 rounded-md cursor-pointer mx-auto"
+          type="submit"
+        >
+          Signin with GitHub
+        </button>
+      </form>
+    </div>
   );
 }
 
